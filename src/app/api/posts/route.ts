90720d18fc239bc9e35151752a5a59244803a419
matti909 <matias.saantiago@gmail.com>
2023-04-19 18:53:53 -0300

    2

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,8 +1,8 @@
 import { NextApiResponse } from 'next';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 import prisma from '../../../../prisma/client';
 
 export async function GET(req: NextRequest, res: NextApiResponse) {
   const data = await prisma.post.findMany();
-  return NextResponse.json(data);
+  return res.json(data);
 }
